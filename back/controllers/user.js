02645b6e9d107e801ae278a22c194d72c5fd0af8
Emma-fOT <brossard.e@gmail.com>
2022-03-29 19:10:38 +0200

    Security checks for the email and the password

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -8,20 +8,51 @@ const dotenv = require("dotenv");
 dotenv.config();
 const TOKEN_KEY = process.env.TOKEN_KEY;
 
+const emailvalidator = require("email-validator");
+const passwordValidator = require("password-validator");
+let passwordSchema = new passwordValidator();
+passwordSchema
+  .is()
+  .min(8) // Minimum length
+  .is()
+  .max(50) // Maximum length
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(1) // Must have at least x digits
+  .has()
+  .not()
+  .spaces() // Should not have spaces
+  .has()
+  .symbols(1); // Must have at least x symbols
+
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
+  if (!emailvalidator.validate(req.body.email)) {
+    return res.status(401).json({ error: "Email non valide !" });
+  } else {
+    if (!passwordSchema.validate(req.body.password)) {
+      return res.status(401).json({
+        error:
+          "Mot de passe trop faible : il doit contenir au moins 8 caractères (dont au moins un chiffre, une majuscules, une minuscules et un caractère spécial) !",
       });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+    } else {
+      bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+          const user = new User({
+            email: req.body.email,
+            password: hash,
+          });
+          user
+            .save()
+            .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+            .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
+    }
+  }
 };
 
 exports.login = (req, res, next) => {
